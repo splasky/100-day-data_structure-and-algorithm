f48aa26cd4e469ab155df3588b90dc60b2f89c14
Splasky <henrychung860326@gmail.com>
2017-09-26 08:23:04 +0800

    Add new function for link list.

--- data_structure/list.h
+++ data_structure/list.h
@@ -1,6 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdlib.h>
+
 typedef struct Node_ {
     void* data;
     struct _Node* prev;
@@ -11,12 +13,24 @@ typedef struct List_ {
     int size;
     Node* tail;
     Node* head;
-    void (*destory)(struct List_** list);
-
+    void (*destory)(struct List_* list);
 } List;
 
-List* init_link_list(List** list);
-List* insert_node(List** list, void* data);
-void destory(List** list);
+List* init_link_list(List* list);
+void list_destory(List* list);
+void list_remove(List* list, Node node);
+void list_push(List* list, Node node);
+void* list_pop(List* list);
+void* list_peek(List* list);
 
+/*
+ * L:list
+ * S:first node
+ * M:next node
+ * V:current node
+ */
+#define foreach(L, S, M, V)                                                              \
+    Node* _node = NULL;                                                                  \
+    Node* V = NULL;                                                                      \
+    for (V = _node = L->S; _node != NULL; V = _node = _node->M)
 #endif /* ifndef LIST_H */
