359eae1b1795d44c5a235c68049ffec3b7a4e5c8
Splasky <henrychung860326@gmail.com>
2017-10-26 17:54:13 +0800

    Add maze.

---
+++ data_structure/tests/test_maze.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* origin 1:wall,0 path */
+
+#define maze_size (8)
+static int maze[maze_size][maze_size] = { { 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 0, 1, 0, 0, 0, 0, 0 }, { 1, 0, 1, 0, 0, 1, 0, 1 }, { 1, 0, 1, 0, 0, 1, 1, 1 },
+    { 1, 0, 0, 0, 0, 1, 0, 1 }, { 1, 1, 0, 0, 0, 0, 0, 1 }, { 1, 1, 1, 1, 0, 1, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1 } };
+
+int findPath(int x, int y)
+{
+    /* congrulation!
+     * finish
+     */
+
+    if (x == 0 && y == 0) {
+        maze[x][y] = 2;
+        return 1;
+    }
+
+    else if (maze[x][y] == 0) {
+        maze[x][y] = 2;
+        if ((findPath(x - 1, y) + findPath(x + 1, y) + findPath(x, y - 1)
+                + findPath(x, y + 1))
+            > 0) {
+            return 1;
+        } else {
+            /* oh no~ */
+            maze[x][y] = 0;
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+void print_maze(void)
+{
+    for (int i = 0; i < maze_size; i++) {
+        for (int j = 0; j < maze_size; j++) {
+            printf("%d ", maze[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    printf("origin:\n");
+    print_maze();
+    printf("after:\n");
+    findPath(6, 6);
+    return 0;
+}
