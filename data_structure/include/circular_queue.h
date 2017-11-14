d8051452a9a3c8ab4f7c518c7c125de01b6466ac
Splasky <henrychung860326@gmail.com>
2017-11-14 13:08:30 +0800

    Move #define marco before function defination.

--- data_structure/include/circular_queue.h
+++ data_structure/include/circular_queue.h
@@ -10,17 +10,18 @@ struct __circular_Queue {
     int max;
 };
 
-Circular_Queue* New_Circular_Queue(const int max);
 #define is_Circular_Queue_Full(cqueue) ((cqueue->max == Queue_count(cqueue->base_queue)))
 #define is_Circular_Queue_Empty(cqueue)                                                  \
     ((cqueue->base_queue->head == NULL) ? true : false)
 #define Circular_Queue_count(cqueue) Queue_count(cqueue->base_queue)
+#define Circular_Queue_destory_and_clean(cqueue)                                         \
+    (Circular_Queue_clear(cqueue));                                                      \
+    (Circular_Queue_destory(cqueue));
+
+Circular_Queue* New_Circular_Queue(const int max);
 int Circular_Queue_enqueue(Circular_Queue* cqueue, void* value);
 void* Circular_Queue_dequeue(Circular_Queue* cqueue);
 void Circular_Queue_destory(Circular_Queue* cqueue);
 void Circular_Queue_clear(const Circular_Queue* cqueue);
-#define Circular_Queue_destory_and_clean(cqueue)                                         \
-    (Circular_Queue_clear(cqueue));                                                      \
-    (Circular_Queue_destory(cqueue));
 
 #endif /* end of include guard: CIRCULAR_QUEUE_H_YQ0ROWOP */
