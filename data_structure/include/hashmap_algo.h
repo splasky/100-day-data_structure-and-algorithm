a87d8bfb84c278fb52d063859d36a410185ee4be
Splasky <henrychung860326@gmail.com>
2018-01-29 23:01:25 +0800

    Add hash function algorithm

---
+++ data_structure/include/hashmap_algo.h
@@ -0,0 +1,8 @@
+#ifndef HASHMAP_ALGO_H_JIFZA587
+#define HASHMAP_ALGO_H_JIFZA587
+
+#include <stdint.h>
+
+uint32_t Hashmap_fnv1a_hash(void* data);
+uint32_t Hashmap_djb_hash(void* data);
+#endif /* end of include guard: HASHMAP_ALGO_H_JIFZA587 */
---
+++ data_structure/src/hashmap_algo.c
@@ -0,0 +1,31 @@
+#include "../include/hashmap_algo.h"
+#include <stdlib.h>
+#include <string.h>
+
+const uint32_t FNV_PRIME = 16777619;
+const uint32_t FNV_OFFSET_BASIS = 2166136261;
+
+uint32_t Hashmap_fnv1a_hash(void* data)
+{
+    const char* s = (char*)data;
+    uint32_t hash = FNV_OFFSET_BASIS;
+    const size_t str_len = strlen(s);
+    for (int i = 0; i < str_len; ++i) {
+        hash ^= *s;
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+}
+
+uint32_t Hashmap_djb2_hash(void* data)
+{
+    uint32_t hash = 5381;
+    int c = *(char*)data;
+
+    while (c) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        c = *(char*)data++;
+    }
+    return hash;
+}
