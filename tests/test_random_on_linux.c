478c9bf4678477c67f3e35d72c409147f1a72475
splasky <henrychung860326@gmail.com>
2018-08-31 20:06:07 +0800

    Fix warning

--- tests/test_random_on_linux.c
+++ tests/test_random_on_linux.c
@@ -6,10 +6,13 @@ TEST(test_random)
     unsigned int rand_num = 0x0;
     FILE* f;
     f = fopen("/dev/random", "r");
-    fread(&rand_num, sizeof(rand_num), 1, f);
+    size_t ret = fread(&rand_num, sizeof(rand_num), 1, f);
     fclose(f);
 
-    printf("%u\n", rand_num);
+    if (ret == sizeof(rand_num))
+    {
+        printf("%u\n", rand_num);
+    }
 
     return NULL;
 }
